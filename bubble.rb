40b7bc7a09b3edf28e1449719fd0b6856b78b3a9
James McCann <jamesredux@gmail.com>
2017-05-23 21:08:39 +0700

    bubble.rb updated

--- bubble.rb
+++ bubble.rb
@@ -7,29 +7,39 @@ def bubble_sort(array)
 				next run through - don't have to check last element but do the same as before
 				keep running through until array is sorted.
 =end
-	sorted = false
-	 #until sorted
-	 	array.each_with_index do |x, i|
-	 		next_ell = i + 1
-	 		
-	 		if x > array[next_ell]
-	 			puts "yes"
 
-	 			puts array
-	 		else 
-	 		 sorted = true	
-	 		end #if
+sorted = false
+check_it = array.length - 1
+until sorted
+		needswap = 0  #so it is reset at the beginning of each run thru
+	check_it.times do |i|
+		if array[i] > array[i+1]
+			array[i], array[i+1] = array[i+1], array[i]
+			needswap = 1
+		end
+		
+		puts array.inspect
+		if needswap == 0
+			sorted = true
+			
+		end
+	end
+	 check_it -= 1 
+	if check_it == 0
+		sorted = true
+	end
 
-	 	end	 #each-do
-	 #end  #until
+ end
 end #whole method
 
-#def bubble_sort_by
-#	2nd problem
-#end
-
-bubble_sort([2,1])
 
 
-#problem at the moment is it goes to last element of array and looks for something to the right - gets nil 
-#can't compare a number to nil so gives error --
+bubble_sort([35,1,2,3,4,5])
+
+#this works but needs a break for when numbers are in the right order - no swaps 
+#needswap is the break starts at 0 during run through if a swap is needed it is set to 1 -
+# if no swaps are needed in a run then it will remain at 0 - this must mean that the array is sorted 
+# so sorted is set to true 
+
+#sorted is also set to true when check it reaches 0 as the array MUST to in order by that point as 
+#each pair has been gone through the maximum number of times
